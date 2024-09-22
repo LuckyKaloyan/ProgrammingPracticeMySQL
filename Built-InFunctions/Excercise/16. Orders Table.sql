ef1178b10fe5c14ce43287a4e757ec903ecd9ef5
Калоян Димитров <136224811+LuckyKaloyan@users.noreply.github.com>
2024-09-22 22:10:22 +0300

    Update

--- Built-InFunctions/Excercise/16. Orders Table.sql
+++ Built-InFunctions/Excercise/16. Orders Table.sql
@@ -1,1 +1,1 @@
-SELECT product_name,order_date, DATE_ADD(order_date, INTERVAL 3 DAY) AS 'pay_due', DATE_ADD(order_date, INTERVAL 30 DAY) AS 'deliver_due' FROM orders;
+SELECT product_name,order_date, DATE_ADD(order_date, INTERVAL 3 DAY) AS 'pay_due', DATE_ADD(order_date, INTERVAL 1 MONTH) AS 'deliver_due' FROM orders;
